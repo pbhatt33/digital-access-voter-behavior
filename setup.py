241e68b88e919f7925345685f36c817068b6f540
Pragna Bhatt <pbhatt3@luc.edu>
2022-12-05 21:36:12 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project explores how access to the internet affected voter behavior in the 2016 election.',
+    author='Pragna Bhatt',
+    license='',
+)
